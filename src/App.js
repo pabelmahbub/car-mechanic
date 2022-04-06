9067ac69b26f61194de40df8071668b7b77b70c6
pabelmahbub <pabelmahbub@gmail.com>
2022-04-07 00:08:10 +0900

    React router started

--- src/App.js
+++ src/App.js
@@ -2,11 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import Services from './Pages/Home/Services/Services';
 import Home from './Pages/Home/Home/Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NotFound from './Pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+    <div className="container">
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+        <Route path="*">
+          <NotFound />
+        </Route>
+       
+      </Switch>
+    </div>
+  </Router>
     </div>
   );
 }
