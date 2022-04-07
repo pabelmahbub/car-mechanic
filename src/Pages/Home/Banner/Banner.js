fd7e68b6cf8c85819b63e3f0d9b690fdc59b7c5c
pabelmahbub <pabelmahbub@gmail.com>
2022-04-07 12:20:35 +0900

    for carousel used React slick to display images

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,48 +1,111 @@
+// import React from 'react';
+// import {Carousel} from 'react-bootstrap';
+
+// //for slick
+
+// import "~slick-carousel/slick/slick.css"; 
+// import "~slick-carousel/slick/slick-theme.css";
+
+// function Banner() {
+//   return (
+//     <>
+//     <Carousel>
+//   <Carousel.Item>
+//     <img
+//       className="d-block w-100"
+//       src="https://img.freepik.com/free-vector/auto-repair-shop-service-workers-fixing-car-and-billing-customer-retro-cartoon-indoor-composition_1284-16849.jpg?w=996" height={500}
+//       alt="First slide"
+//     />
+//     <Carousel.Caption>
+//       <h3>MAKE YOUR CAR LAST LONGER</h3>
+//       <p>JUST A PHONE CALL NEEDED</p>
+//     </Carousel.Caption>
+//   </Carousel.Item>
+//   <Carousel.Item>
+//     <img
+//       className="d-block w-100"
+//       src="https://img.freepik.com/free-photo/close-up-hands-of-unrecognizable-mechanic-doing-car-service-and-maintenance_146671-19689.jpg?t=st=1649255003~exp=1649255603~hmac=2f12f256ac7c92150c576d1149f10882e7cfb4dd8859877ccab83ef4c2466c2c&w=900" height={500}
+//       alt="Second slide"
+//     />
+
+//     <Carousel.Caption>
+//       <h3>MAKE YOUR JOURNEY SAFE AND RELAXING</h3>
+//       <p>JUST A PHONE CALL NEEDED.</p>
+//     </Carousel.Caption>
+//   </Carousel.Item>
+//   <Carousel.Item>
+//     <img
+//       className="d-block w-100"
+//       src="https://img.freepik.com/free-vector/auto-repair-shop-service-workers-fixing-car-and-billing-customer-retro-cartoon-indoor-composition_1284-16849.jpg?w=996" height={500}
+//       alt="Third slide"
+//     />
+
+//     <Carousel.Caption>
+//       <h3>Third slide label</h3>
+//       <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+//     </Carousel.Caption>
+//   </Carousel.Item>
+// </Carousel>
+// </>
+//   )
+// }
+
+// export default Banner;
+
+
+
+
+// //for slick
 import React from 'react';
-import {Carousel} from 'react-bootstrap'
+
+import Slider from "react-slick";
 
 function Banner() {
-  return (
-    <>
-    <Carousel>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://img.freepik.com/free-vector/auto-repair-shop-service-workers-fixing-car-and-billing-customer-retro-cartoon-indoor-composition_1284-16849.jpg?w=996" height={500}
-      alt="First slide"
-    />
-    <Carousel.Caption>
-      <h3>First slide label</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://img.freepik.com/free-photo/close-up-hands-of-unrecognizable-mechanic-doing-car-service-and-maintenance_146671-19689.jpg?t=st=1649255003~exp=1649255603~hmac=2f12f256ac7c92150c576d1149f10882e7cfb4dd8859877ccab83ef4c2466c2c&w=900" height={500}
-      alt="Second slide"
-    />
 
-    <Carousel.Caption>
-      <h3>Second slide label</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      src="https://img.freepik.com/free-vector/auto-repair-shop-service-workers-fixing-car-and-billing-customer-retro-cartoon-indoor-composition_1284-16849.jpg?w=996" height={500}
-      alt="Third slide"
-    />
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    backgroundColor:'red'
+  };
 
-    <Carousel.Caption>
-      <h3>Third slide label</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
+   return (
+     <>
+
+       <div>
+        <h2> Single Item</h2>
+        <Slider {...settings}>
+          <div style={{}}>
+          <img
+       className="d-block w-100"
+       src="https://img.freepik.com/free-vector/auto-repair-shop-service-workers-fixing-car-and-billing-customer-retro-cartoon-indoor-composition_1284-16849.jpg?w=996" height={400}
+       alt="First slide"
+       />
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </div>
+
 </>
   )
-}
+ }
 
-export default Banner
+ export default Banner;
+
+
