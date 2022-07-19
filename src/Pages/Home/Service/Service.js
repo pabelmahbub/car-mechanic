b5726d1a2b7615979d0ff563afae26106c6334e7
レヴィアス株式会社 <revuiasukabushikikaisha@revuiasukabushikikaishas-MacBook-Air.local>
2022-07-19 17:45:37 +0900

    fixe expert json file

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -3,14 +3,14 @@ import { Link } from 'react-router-dom';
 import './Service.css'
 
 function Service({service}) {
-    const{_id,name,img,price,time,des}=service;
+    const{_id,name,img,price,description}=service;
   return (
     <div className='service pb-3' >
          <img src={img} alt='' width={420} height={250}></img>
         <h1 style={{fontSize:20,paddingTop:6}}>Service name:{name}</h1>
-        <h2 style={{fontSize:24}}>Service Charge: ${price}</h2>
-        <h5 style={{paddingRight:10,paddingLeft:10,fontSize:13}}>{des}</h5>
-        <h2 style={{fontSize:24,color:'royalblue'}}>Time duration:{time} hour</h2>
+       
+        <h5 style={{paddingRight:10,paddingLeft:10,fontSize:13}}>{description}</h5>
+        <h2 style={{fontSize:24}}>Service Charge: ¥{price}</h2>
         <Link to={`/booking/${_id}`}>
         <button className='btn btn-info'>Book {name.toLowerCase()}</button>
         
